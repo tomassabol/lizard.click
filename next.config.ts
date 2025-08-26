27eef5ac94aa6bbe8774b8e09245e66d03195b1b
Tomáš Sabol <tomassabol2@gmail.com>
2025-08-26 23:34:10 +0200

    Enable react compiler

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import "~/env";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
